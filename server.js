dab3c09bf299c572b258341abea740626fb491cf
AleksAugusts <aleksaugustsstrazdins@gmail.com>
2025-03-29 17:13:46 +0200

    Pievienoti server.js un package.json faili

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post("/webhook", (req, res) => {
+  console.log("Webhook received!");
+  let body = req.body;
+
+  if (body.object === "page") {
+    body.entry.forEach((entry) => {
+      let messaging = entry.messaging;
+      messaging.forEach((message) => {
+        console.log(message);
+      });
+    });
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.get("/webhook", (req, res) => {
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  if (mode && token) {
+    if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running on port ${port}`);
 });
-
